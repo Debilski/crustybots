f21b5dfbdd4d73bfe6af1582c4ca1740d7bd5b8c
Rike-Benjamin Schuppner <debilski@debilski.de>
2023-08-14 20:55:52 +0200

    First crustybots version.

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+
+use pelita_rust_wrapper::*;
+
+use pyo3::prelude::PyObject;
+
+use rand::seq::SliceRandom;
+
+const TEAM_NAME: &str = "Team rusty lantern";
+
+fn movebot<T>(bot: Bot, state: T) -> Pos {
+    // TODO: Our state should probably be a &mut Option<T> that starts out None
+
+    // fetch our legal positions
+    let legal_pos = bot.legal_positions;
+
+    // choose a random one
+    let dice = legal_pos.choose(&mut rand::thread_rng());
+
+    // unwrap the option (we are sure that there is at least one move possible)
+    let pos: Pos = *dice.unwrap();
+
+    // return
+    pos
+}
+
+pelita_player!(crustybots, TEAM_NAME, movebot);
