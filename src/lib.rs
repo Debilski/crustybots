6c1180cbf2d20108ba33a7c1db2ade3862d5768a
Rike-Benjamin Schuppner <debilski@debilski.de>
2023-08-15 20:50:47 +0200

    Showing off grid.distance, speaking

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,33 @@ use pyo3::prelude::PyObject;
 
 use rand::seq::SliceRandom;
 
+use pathfinding::grid::Grid;
+
 const TEAM_NAME: &str = "Team rusty lantern";
 
-fn movebot<T>(bot: Bot, state: T) -> Pos {
+fn create_grid(bot: &Bot) -> Grid {
+    let (w, h) = bot.shape;
+    let mut g = Grid::new(w, h);
+    for &elem in bot.walls.iter() {
+        g.add_vertex(elem);
+    }
+    g.invert();
+    g
+}
+
+fn movebot<T>(bot: &Bot, state: T) -> Pos {
     // TODO: Our state should probably be a &mut Option<T> that starts out None
 
+    let grid = create_grid(&bot);
+    print!("{:?}", grid);
+
+    let food_distance: Vec<_> = bot.enemy[0].food.iter().map(|&pos| grid.distance(bot.position, pos)).collect();
+    bot.say("Hello");
+
+    print!("{:?}", food_distance);
+
     // fetch our legal positions
-    let legal_pos = bot.legal_positions;
+    let legal_pos = &bot.legal_positions;
 
     // choose a random one
     let dice = legal_pos.choose(&mut rand::thread_rng());
@@ -22,6 +42,7 @@ fn movebot<T>(bot: Bot, state: T) -> Pos {
     // return
     pos
 }
+
 
 // Register player (package name, team name, move fn)
 // NB: The package name needs to be the same as in pyproject.toml
